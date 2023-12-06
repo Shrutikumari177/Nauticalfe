175c3ab232ef7747dd90633d4f2d4fe109e5d6eb
Shruti Kumari <shrutikumari0811@gmail.com>
2023-12-06 06:36:23 +0000

    item detail section added

--- webapp/controller/TrChangeVoyage.controller.js
+++ webapp/controller/TrChangeVoyage.controller.js
@@ -8,9 +8,13 @@ sap.ui.define(
       return BaseController.extend("nauticalfe.controller.TrChangeVoyage", {
         onInit() {
           var hideButton = this.byId("Hide");
+          var hideButton1 = this.byId("Hide1");
           if (hideButton) {
                 hideButton.attachPress(this.toggleNavContainer.bind(this));
           }
+          if (hideButton1) {
+            hideButton1.attachPress(this.toggleBarAndNavContainer.bind(this));
+        }
           
 
         },
@@ -24,6 +28,19 @@ sap.ui.define(
               navCon.back();
           }
       },
+        handleNavToPanelA: function() {
+        this.navigateToPanel("panelA");
+       },
+
+       handleNavToPanelB: function() {
+        this.navigateToPanel("panelB");
+      },
+
+    navigateToPanel: function(panelId) {
+        var navCon = this.byId("navCon2");
+        navCon.to(this.byId(panelId));
+    },
+
           toggleNavContainer: function() {
             var navCon = this.byId("navCon");
             var bar = this.byId("HBox10");
@@ -35,7 +52,15 @@ sap.ui.define(
             bar.setVisible(!currentVisibility);
             
 
-          }
+          },
+          toggleBarAndNavContainer: function() {
+            var navCon2 = this.byId("navCon2");
+            var bar2 = this.byId("HBox20");
+            var currentVisibility = navCon2.getVisible();
+
+            navCon2.setVisible(!currentVisibility);
+            bar2.setVisible(!currentVisibility);
+        }
       });
     }
   );
