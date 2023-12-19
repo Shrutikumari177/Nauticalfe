f7aaf2d0b358d45c8c56fbf15b9079a909af39cb
Shruti Kumari <shrutikumari0811@gmail.com>
2023-12-19 07:07:46 +0000

    navigation updated

--- webapp/controller/MasterVoyageType.controller.js
+++ webapp/controller/MasterVoyageType.controller.js
@@ -7,9 +7,11 @@ sap.ui.define(
       "sap/ui/model/odata/v4/Context",
       "sap/ui/model/odata/v4/ODataContextBinding",
       "sap/m/MessageBox",
-      "sap/m/MessageToast"
+      "sap/m/MessageToast",
+      "sap/ui/core/routing/History",
+      "sap/ui/core/Fragment"
     ],
-    function (BaseController, ODataModel, Context, MessageBox, ODataContextBinding, MessageToast) {
+    function (BaseController, ODataModel, Context, MessageBox, ODataContextBinding, MessageToast,History,Fragment) {
       "use strict";
       let aSelectedIds = [];
       var emptyrowAdded=0;
@@ -433,7 +435,44 @@ sap.ui.define(
               }
             });
           });
+        },
+        onExit: function () {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("MastView");
+        },
+        onBackPressHome: function () {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("RouteView1");
+        },
+        backPress:function(){
+          const oHistory = History.getInstance();
+          const sPreviousHash = oHistory.getPreviousHash();
+
+          if (sPreviousHash !== undefined) {
+            window.history.go(-1);
+          } else {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("MastView", {}, true);
+          }
+       },
+       onPress: function () {
+        var oView = this.getView(),
+          oButton = oView.byId("button");
+        if (!this._oMenuFragment) {
+          this._oMenuFragment = Fragment.load({
+            name: "nauticalfe.fragments.MastOptionsDropDown",
+                        id: oView.getId(),
+            controller: this
+          }).then(function(oMenu) {
+            oMenu.openBy(oButton);
+            this._oMenuFragment = oMenu;
+            return this._oMenuFragment;
+          }.bind(this));
+        } else {
+          this._oMenuFragment.openBy(oButton);
         }
+      }
+
   
       });
   
